d8128ae84580e268e822392fec09b16df3b30656
wangfulong <wangfulong@flowcapai.com>
2022-02-01 16:28:38 +0800

    update

--- src/react-demos/add-react-in-one-minute/like_button.js
+++ src/react-demos/add-react-in-one-minute/like_button.js
@@ -13,13 +13,13 @@ class LikeButton extends React.Component {
       return "You liked this.";
     }
 
-    return e(
-      "button",
-      { onClick: () => this.setState({ liked: true }) },
-      "Like"
-    );
+    // 显示一个 "Like" <button>
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
   }
 }
 
-const domContainer = document.querySelector("#like_button_container");
-ReactDOM.render(e(LikeButton), domContainer);
+document.querySelectorAll(".like_button_container").forEach((domContainer) => {
+  // Read the comment ID from a data-* attribute
+  const commentID = parseInt(domContainer.dataset.commentid, 10);
+  ReactDOM.render(e(LikeButton, { commentID: commentID }), domContainer);
+});
